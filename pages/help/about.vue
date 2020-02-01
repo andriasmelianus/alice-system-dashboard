c9cf58f17f366a80887ccd516d620a59b6284666
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2020-02-01 21:42:15 +0700

    Perbaikan kecil pada halaman under-construction.vue dan about.vue.

--- pages/help/about.vue
+++ pages/help/about.vue
@@ -1,7 +1,14 @@
 <template>
-  <v-row align="center" justify="center">
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <div style="max-width:600px;">
+        <div class="display-4 font-weight-thin">{{ title }}</div><br>
+        <div class="font-weight-light">{{ description }}</div><br>
+        <div class="overline">Copyright {{ title }} &copy; 2019</div>
+      </div>
+    </v-row>
 
-  </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -10,12 +17,13 @@ export default {
   layout: 'dashboard',
   head() {
     return {
-      title: 'Tentang ' + this.title
+      title: 'Tentang'
     }
   },
 
   data: () => ({
-    title: process.env.appName
+    title: process.env.appName,
+    description: process.env.appDescription,
   })
 }
 </script>
--- pages/under-construction.vue
+++ pages/under-construction.vue
@@ -1,22 +1,27 @@
 <template>
-  <v-row justify="center">
-    <h3>Segera datang...</h3>
-    <p>Mohon maaf, halaman ini masih belum dapat diakses karena masih dalam masa pembuatan.</p>
-  </v-row>
+  <v-container fluid class="fill-height">
+    <v-row justify="center" align="center">
+      <v-col class="text-center">
+        <div class="display-2">Dalam Pembangunan</div>
+        <p
+          class="caption"
+        >Mohon maaf, halaman ini masih belum dapat diakses karena masih dalam masa pembangunan.</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  middleware:'auth',
-  layout: 'dashboard',
+  middleware: "auth",
+  layout: "dashboard",
   head() {
     return {
-      title: '<<< under construction >>>'
-    }
+      title: "<<< under construction >>>"
+    };
   }
-}
+};
 </script>
 
 <style>
-
 </style>
