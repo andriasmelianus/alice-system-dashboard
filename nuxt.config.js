ab5f8e5e59f7fbc5ffc9358247a88dc1af022ebe
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2020-02-01 21:43:37 +0700

    Perbaikan kecil dan mempercantik format pada nuxt.config.js.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,77 +1,83 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
-var appName = 'Alice System'
+var appName = "Alice System",
+  appDescription =
+    "Aplikasi ERP mini yang dibangun dengan memperhatikan kemudahan penggunaan bagi pengguna yang awam sekalipun. Aplikasi ini ditujukan untuk perusahaan dengan skala kecil dan menengah.";
 
 export default {
-  mode: 'universal',
+  mode: "universal",
   /**
    * Environment variables
    */
   env: {
     appName: appName,
+    appDescription: appDescription
   },
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    titleTemplate: '%s - ' + appName,
-    title: process.env.npm_package_name || '',
+    titleTemplate: "%s - " + appName,
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxtjs/vuetify',
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxtjs/vuetify"],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/auth'
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth"],
   auth: {
     strategies: {
       local: {
         endpoints: {
-          login: {url: 'http://gateway.local/auth/login', method: 'post', propertyName: 'token'},
-          logout: {url: 'http://gateway.local/auth/logout', method: 'post'},
-          user: {url: 'http://gateway.local/auth/user', method: 'get', propertyName: false},
+          login: {
+            url: "http://gateway.local/auth/login",
+            method: "post",
+            propertyName: "token"
+          },
+          logout: { url: "http://gateway.local/auth/logout", method: "post" },
+          user: {
+            url: "http://gateway.local/auth/user",
+            method: "get",
+            propertyName: false
+          }
         },
         tokenRequired: true,
-        tokenName: 'Authorization',
-        tokenType: 'Bearer'
+        tokenName: "Authorization",
+        tokenType: "Bearer"
       }
     }
   },
   /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: false,
       themes: {
@@ -88,13 +94,12 @@ export default {
     }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
