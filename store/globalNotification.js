84100b56078d51b2e093afd88807f2361136d409
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2020-02-10 09:47:27 +0700

    Perbaikan penampilan pesan error pada globalNotification.js. Pesan error yang ditampilkan adalah pesan error buatan sendiri. Jika tidak maka pesan error standar HTTP yang akan ditampilkan.

--- store/globalNotification.js
+++ store/globalNotification.js
@@ -1,34 +1,46 @@
 export const state = () => ({
   isShown: false,
-  color: 'success',
-  message: ''
-})
+  color: "success",
+  message: ""
+});
 
 export const getters = {
-  isShown: (state) => {
-    return state.isShown
+  isShown: state => {
+    return state.isShown;
   }
-}
+};
 
 export const mutations = {
-  hide(state){
-    state.isShown = false
+  hide(state) {
+    state.isShown = false;
   },
-  show(state, notification){
+  show(state, notification) {
     //isShown nilai-nya pasti true pada saat pemanggilan
-    state.isShown = true
-    state.color = notification.color ? notification.color : 'success'
+    state.isShown = true;
+    state.color = notification.color ? notification.color : "success";
 
-    let rawMessage = notification.message
+    let rawMessage = notification.message;
     // if(rawMessage.request){
     //   state.message = 'HTTP Error [' + rawMessage.request.status + ']: ' + rawMessage.request.statusText
     // }else if(rawMessage.response){
-    if(rawMessage.response){
-      state.message = 'HTTP Error [' + rawMessage.response.status + ']: ' + rawMessage.response.statusText
-    }else if(typeof(rawMessage) == 'string'){
-      state.message = rawMessage
-    }else{
-      state.message = rawMessage.message
+    if (rawMessage.response) {
+      if (typeof rawMessage.response.data.error == "string") {
+        state.message =
+          "HTTP Error [" +
+          rawMessage.response.status +
+          "]: " +
+          rawMessage.response.data.error;
+      } else {
+        state.message =
+          "HTTP Error [" +
+          rawMessage.response.status +
+          "]: " +
+          rawMessage.response.statusText;
+      }
+    } else if (typeof rawMessage == "string") {
+      state.message = rawMessage;
+    } else {
+      state.message = rawMessage.message;
     }
   }
-}
+};
