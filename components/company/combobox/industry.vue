b122c9aed836f0b1752fe1a0b04ee94914ae4223
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2020-03-24 19:50:11 +0700

    Pembuatan combobox company business dan company industry.

---
+++ components/company/combobox/industry.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-combobox
+    @update:search-input="handleInput"
+    label="Industri"
+    item-text="name"
+    item-value="name"
+    :items="records"
+    :search-input.sync="search"
+    :error-messages="errorMessages"
+    :no-data-text="noDataText"
+    :loading="isLoading"
+    no-filter
+    append-icon
+    auto-select-first
+    hide-no-data
+    clearable
+  ></v-combobox>
+</template>
+
+<script>
+import { globalCombobox } from "~/components/_mixin/global-combobox";
+export default {
+  mixins: [globalCombobox],
+
+  data: () => ({
+    apiUrl: "/company-industry"
+  })
+};
+</script>
