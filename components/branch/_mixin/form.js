1255f01f73052684253fe4dba1b83657c6b052bd
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2020-03-24 20:45:50 +0700

    Pembuatan komponen branch.

---
+++ components/branch/_mixin/form.js
@@ -0,0 +1,20 @@
+import { globalForm } from "~/components/_mixin/global-form";
+import { defaultRecord, defaultRecordError } from "./record";
+export const branchForm = {
+  mixins: [globalForm],
+
+  props: {
+    record: {
+      type: Object,
+      default: function() {
+        return defaultRecord;
+      }
+    }
+  },
+  data: () => ({
+    defaultRecordError: defaultRecordError,
+    recordError: defaultRecordError,
+
+    apiUrl: "/branch"
+  })
+};
---
+++ components/branch/widget/link-one.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card hover max-width="250px" nuxt to="/branch">
+    <v-list-item>
+      <v-list-item-avatar>
+        <v-icon dense x-large>mdi-domain</v-icon>
+      </v-list-item-avatar>
+
+      <v-list-item-content>
+        <v-list-item-title>Cabang</v-list-item-title>
+        <v-list-item-subtitle>Kelola data cabang</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
