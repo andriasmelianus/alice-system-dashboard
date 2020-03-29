2bc6b416f27148951192eef650cebf9d269f261e
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2020-03-29 23:08:50 +0700

    Pembuatan autocomplete untuk input kontak (alamat, kota, propinsi dan negara).

---
+++ components/contact/combobox/country.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-combobox
+    label="Negara"
+    item-text="name"
+    item-value="name"
+    @update:search-input="handleInput"
+    :value="value"
+    :search-input.sync="search"
+    :items="records"
+    :error-messages="errorMessages"
+    :no-data-text="noDataText"
+    :loading="isLoading"
+    no-filter
+    append-icon
+    auto-select-first
+    hide-no-data
+    clearable
+  ></v-combobox>
+</template>
+
+<script>
+import { globalCombobox } from "~/components/_mixin/global-combobox";
+export default {
+  mixins: [globalCombobox],
+
+  data: () => ({
+    apiUrl: "/contact/country"
+  })
+};
+</script>
