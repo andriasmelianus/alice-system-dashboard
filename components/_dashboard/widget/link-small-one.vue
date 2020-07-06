aeae5185dbb758869594e3a5e81ace7d4f63438b
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2020-07-06 13:32:33 +0700

    Pembuatan widget link kecil dashboard.

---
+++ components/_dashboard/widget/link-small-one.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-btn class="ma-2" color="grey lighten-3" link nuxt to="/">
+    <v-icon>mdi-gauge</v-icon>&nbsp;
+    Dashboard
+  </v-btn>
+</template>
+<script>
+export default {};
+</script>
